0b30b6e4cd09840a2ff0de5428ccd04929b2981d
duongkien27 <72027801+duongkien27@users.noreply.github.com>
2022-11-23 14:57:46 +0700

    Create User.cpp

---
+++ Groupmatch/User.cpp
@@ -0,0 +1,19 @@
+void init(int N)
+{
+
+}
+
+void updateScore(int mWinnerID, int mLoserID, int mScore)
+{
+
+}
+
+void unionTeam(int mPlayerA, int mPlayerB)
+{
+
+}
+
+int getScore(int mID)
+{
+	return 0;
+}
