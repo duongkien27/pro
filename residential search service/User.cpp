84a07bb3ef72c4dadcaa3d0125702a5ad7bee6fe
duongkien27 <72027801+duongkien27@users.noreply.github.com>
2022-11-23 15:03:05 +0700

    Create User.cpp

---
+++ residential search service/User.cpp
@@ -0,0 +1,16 @@
+void init(int N, int mDownTown[])
+{
+
+}
+void newLine(int M, int mCityIDs[], int mDistances[])
+{
+
+}
+void changeLimitDistance(int mLimitDistance)
+{
+
+}
+int findCity(int mOpt, int mDestinations[])
+{
+	return -1;
+}
