4f85e0bd22285ed94407e30d93bf8e8bccb55bbc
duongkien27 <72027801+duongkien27@users.noreply.github.com>
2022-11-24 16:32:19 +0700

    Create User.cpp

---
+++ Login Waiting List/User.cpp
@@ -0,0 +1,22 @@
+
+void init()
+{
+}
+
+void loginID(char mID[10])
+{
+}
+
+int closeIDs(char mStr[10])
+{
+	return -1;
+}
+
+void connectCnt(int mCnt)
+{
+}
+
+int waitOrder(char mID[10])
+{
+	return -1;
+}
